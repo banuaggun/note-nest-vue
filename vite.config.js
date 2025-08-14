c557dd271563bb82ca64dec02f6db037b32b0845
Banu Ağgün <banuaggun@outlook.com>
2025-08-14 14:45:11 +0300

    Added @ alias to simplify imports from the src directory

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path' // Bu satırı ekle
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // Alias tanımı
+    },
+  },
 })
