251d7b5763529592031fa3fe2fa1f6a9c6f98b7a
Banu Ağgün <banuaggun@outlook.com>
2025-08-15 00:04:49 +0300

    Header area creation started

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="header">
+    <div class="logo">
+      <img :src="logo" alt="Note Nest Logo" />
+    </div>
+    
+  </header>
+</template>
+
+<script setup>
+import logo from '@/assets/image/logo.svg'
+</script>
+
+<style scoped>
+.header {
+  width:100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #fff;
+  height:100px;
+  box-shadow: 0px 4px 8px 0 rgba(29, 34, 38, 0.45);
+  margin-bottom:12px;
+  position:fixed;
+  z-index:10;
+}
+
+.logo img {
+  height: 40px;
+}
+
+</style>
