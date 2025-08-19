e2b55dc0660618ba82e4bb12300895cba0b5acaa
Banu Ağgün <banuaggun@outlook.com>
2025-08-20 00:38:35 +0300

    Editing toolbar content

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="button-group">
+            <button class="btn" @click="emit('new-note')" >
+                <IconPencil/>
+            </button>
+            <button class="btn">
+                <IconBold />
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import IconBold from "./icons/editor/IconBold.vue"
+import IconPencil from "./icons/editor/IconPencil.vue"
+
+const emit = defineEmits(['new-note']);
+</script>
+
+<style scoped>
+
+</style>
