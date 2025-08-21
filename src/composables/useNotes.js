4f4c32fc1e5fd84aac67d76298fddb2b286ad68b
Banu Ağgün <banuaggun@outlook.com>
2025-08-21 14:29:22 +0300

    Functions for Crud operations have been edited

--- src/composables/useNotes.js
+++ src/composables/useNotes.js
@@ -1,40 +1,72 @@
 import { ref, computed } from 'vue'
 
-const notes = ref([])
+//const notes = ref([])
 
-const updateStorage = () => {
-  localStorage.setItem('notes-data', JSON.stringify(notes.value))
-}
-
-const loadNotesFromJson = async () => {
-  try {
-    const response = await fetch('/notes.json')
-    const data = await response.json()
-    notes.value = Array.isArray(data) ? data : data.notes || []
-    updateStorage()
-  } catch (error) {
-    console.error('JSON dosyası yüklenemedi:', error)
-  }
-}
-
-const createNote = () => {
-  notes.value.unshift({
-    id: Math.floor(Math.random() * 1000),
-    title: '',
-    content: '',
-    updated: Date.now()
-  })
-  updateStorage()
-}
-
-const allNotes = computed(() =>
-  notes.value.sort((a, b) => b.updated - a.updated)
+const notes = ref(
+  JSON.parse(localStorage.getItem('notes-data')) || []
 )
 
-export default function useNotes() {
-  return {
-    createNote,
-    allNotes,
-    loadNotesFromJson
+const activeNoteId = ref(null)
+
+export default function useNotes(){
+
+  const updateStorage = () => localStorage.setItem('notes-data', JSON.stringify(notes.value))
+
+  const createNote = () => {
+
+    const id = Math.floor(Math.random()*1000)
+
+    notes.value.unshift({
+      id:id,
+      title:'Untitled',
+      body:'',
+      updated: Date.now()
+    })
+
+    activeNoteId.value = id
+
+    updateStorage()
+  }
+
+  const activeNote = computed(() => notes.value.find(note => note.id === activeNoteId.value))
+
+
+  const allNotes = computed(() => notes.value.sort((a, b) => b.updated - a.updated))
+
+  const setActiveNoteId = id => activeNoteId.value = id
+
+  const updateNote = () => {
+    activeNote.value.updated = Date.now()
+    updateStorage()
+  }
+
+  const removeNote = () => {
+    const index = notes.value.findIndex(note => note.id === activeNote.value.id)
+
+    notes.value = notes.value.filter(note => note.id !== activeNote.value.id)
+
+    updateStorage()
+
+    let newIndex = 0
+
+    if(index <= notes.value.length - 1){
+      newIndex = index
+    }else if(index > notes.value.length + 1){
+      newIndex = index - 1
+    }
+
+    if(newIndex >= 0){
+      setActiveNoteId(notes.value[newIndex].id)
+    }
+  }
+
+  return{
+    createNote, 
+    setActiveNoteId,
+    updateNote,
+    removeNote,
+    allNotes, 
+    activeNote
   }
 }
+
