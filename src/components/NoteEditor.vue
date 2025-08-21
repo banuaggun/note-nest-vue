c543b23c01800f5d41cf4b849f91383624bca103
Banu Ağgün <banuaggun@outlook.com>
2025-08-21 14:32:02 +0300

    Editing operations have been made operational

--- src/components/NoteEditor.vue
+++ src/components/NoteEditor.vue
@@ -1,22 +1,43 @@
 <template>
   <section class="note-editor">
-    <ToolBar @new-note="createNote"/>
-    <input v-model="title" placeholder="Note title" />
-    <textarea v-model="content" placeholder="Write your note..."></textarea>
-    <button @click="saveNote">Save</button>
+    <ToolBar @new-note="createNote" @remove-note="removeActiveNote" :disabled="!activeNote" />
+    <div class="note-editor-content" v-if="activeNote">
+
+      <input type="text" @keyup="updateNote" v-model="activeNote.title" placeholder="Note title" />
+
+      <textarea @keyup="updateNote" v-model="activeNote.content" placeholder="Write your note..."></textarea>
+
+      <!-- 
+        <input type="text" v-debounce:500ms="updateNote" />
+        <textarea v-debounce=500ms="updateNote"></textarea>
+      -->
+
+      <button @click="saveNote">Save</button>
+
+    </div>
+
   </section>
 </template>
 
 <script setup>
 import { ref } from 'vue'
+//import {vueDebounce} from 'vue-debounce';
 import useNotes from '../composables/useNotes';
 import ToolBar from './ToolBar.vue';
 
 
-const {createNote} = useNotes()
+const { createNote, activeNote, updateNote, removeNote } = useNotes()
+
+//const vDebounce = vueDebounce({lock:true})
 
 const title = ref('')
 const content = ref('')
+
+const removeActiveNote = () => {
+  if(confirm("Are you sure?")){
+    removeNote()
+  }
+}
 
 function saveNote() {
   console.log('Note saved:', title.value, content.value)
@@ -25,11 +46,13 @@ function saveNote() {
 
 <style scoped>
 .note-editor {
-  flex:1;
+  flex: 1;
   padding: 1rem;
   border-top: 1px solid #ccc;
 }
-input, textarea {
+
+input,
+textarea {
   width: 100%;
   margin-bottom: 0.5rem;
 }
--- src/components/ToolBar.vue
+++ src/components/ToolBar.vue
@@ -1,12 +1,20 @@
 <template>
     <div>
         <div class="button-group">
-            <button class="btn" @click="emit('new-note')" >
+            <button :disabled="disabled" class="btn" @click="emit('new-note')">
                 <IconPencil/>
             </button>
-            <button class="btn">
+            <button :disabled="disabled" class="btn" @click="emit('new-note')">
                 <IconBold />
             </button>
+        </div>
+
+        <div class="divider">
+            <div>
+                <button class="btn btn-remove" @click="emit('remove-note')">
+                    <i class="ph-fill ph-trash"></i>
+                </button>
+            </div>
         </div>
     </div>
 </template>
@@ -15,9 +23,16 @@
 import IconBold from "./icons/editor/IconBold.vue"
 import IconPencil from "./icons/editor/IconPencil.vue"
 
-const emit = defineEmits(['new-note']);
+const emit = defineEmits(['new-note', 'remove-note']);
+
+defineProps({
+    disabled:Boolean
+})
 </script>
 
 <style scoped>
-
+.btn-remove{
+    color:red;
+    font-size:1.5rem;
+}
 </style>
