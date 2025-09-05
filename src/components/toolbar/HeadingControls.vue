1f4076e8ed99f03fa738ae67046b8008eb083292
Banu Ağgün <banuaggun@outlook.com>
2025-09-05 16:35:36 +0300

    Heading controls have been edited and a single file has been checked in the toolbar. Others will be added later.

--- src/components/toolbar/HeadingControls.vue
+++ src/components/toolbar/HeadingControls.vue
@@ -1,72 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+import { useHeadingMode } from '../../composables/useHeadingMode'
+
+const { activeHeading, toggleHeading } = useHeadingMode()
+</script>
+
 <template>
-  <div class="heading-button-group">
-    <button
-      v-for="level in 6"
-      :key="level"
-      class="btn"
-      @click="emit('format', `h${level}`)"
-      :class="{ active: isFormatActive[`h${level}`] }"
-      :aria-label="`Heading ${level}`"
-    >
-      <component :is="iconMap[`h${level}`]" />
-    </button>
+  <div class="header-controls">
+    <div class="heading-buttons">
+      <button
+        v-for="level in 6"
+        :key="level"
+        :class="{ active: activeHeading === `h${level}` }"
+        @click="toggleHeading(level)"
+      >
+        H{{ level }}
+      </button>
+    </div>
   </div>
 </template>
 
-<script setup>
-import IconH1 from "../icons/editor/IconH1.vue";
-import IconH2 from "../icons/editor/IconH2.vue";
-import IconH3 from "../icons/editor/IconH3.vue";
-import IconH4 from "../icons/editor/IconH4.vue";
-import IconH5 from "../icons/editor/IconH5.vue";
-import IconH6 from "../icons/editor/IconH6.vue";
-
-const emit = defineEmits(["format"]);
-
-defineProps({
-  isFormatActive: {
-    type: Object,
-    required: true
-  }
-});
-
-// İkonları eşleştir
-const iconMap = {
-  h1: IconH1,
-  h2: IconH2,
-  h3: IconH3,
-  h4: IconH4,
-  h5: IconH5,
-  h6: IconH6
-};
-</script>
-
 <style scoped>
-.heading-button-group {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 8px;
-  margin-top: 8px;
-}
-
-.btn {
-  background-color: ivory;
-  border: 1px solid #dcdcdc;
-  color: black;
-  padding: 6px;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
-
-.active {
-  background-color: limegreen;
-  color: black;
-}
-
-svg {
-  width: 20px;
-  height: 20px;
+button.active {
+  background-color: #cce;
+  font-weight: bold;
 }
 </style>
--- src/components/toolbar/ToolBar.vue
+++ src/components/toolbar/ToolBar.vue
@@ -1,27 +1,17 @@
 <template>
-  <div v-if="selectedNote && selectedNote.id">
-    <TextStyleControls :isFormatActive="isFormatActive" @format="emit('format', $event)" />
-    <HeadingControls :isFormatActive="isFormatActive" @format="emit('format', $event)" />
-    <ListControls @format="emit('format', $event)" />
-    <NoteActions :disabled="disabled" :selectedNote="selectedNote" @edit-note="emit('edit-note', $event)" @remove-note="emit('remove-note')" />
-    
-    <div class="note-warning">
-      <p>Click on the edit button to write something.</p>
-    </div>
+  <div>
+     <HeadingControls :note="note" @update="handleUpdate" />
+ 
   </div>
 </template>
 
 <script setup>
-import TextStyleControls from "./TextStyleControls.vue";
 import HeadingControls from "./HeadingControls.vue";
-import ListControls from "./ListControls.vue";
-import NoteActions from "./NoteActions.vue";
 
-const emit = defineEmits(['format', 'edit-note', 'remove-note']);
+defineProps({ note: Object })
+const emit = defineEmits(['update'])
 
-defineProps({
-  disabled: Boolean,
-  selectedNote: Object,
-  isFormatActive: Object
-});
+function handleUpdate(updatedNote) {
+  emit('update', updatedNote)
+}
 </script>
