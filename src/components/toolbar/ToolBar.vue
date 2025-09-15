37897a299c3a3e1b4faedcb4b52d5dcd55c0f189
Banu Ağgün <banuaggun@outlook.com>
2025-09-15 17:52:44 +0300

    Props were defined for bold, italic, and underline styles.

--- src/components/toolbar/ToolBar.vue
+++ src/components/toolbar/ToolBar.vue
@@ -1,24 +1,37 @@
 <script setup>
-import HeadingControls from './HeadingControls.vue'
-import TextStyleControls from './TextStyleControls.vue'
-import SpellcheckToggle from './SpellcheckToggle.vue'
+import HeadingControls from "./HeadingControls.vue";
+import TextStyleControls from "./TextStyleControls.vue";
+import SpellcheckToggle from "./SpellcheckToggle.vue";
 
-defineProps({ note: Object })
-const emit = defineEmits(['update', 'applyStyle'])
+const props = defineProps({
+  note: Object,
+  isBold: Boolean,
+  isItalic: Boolean,
+  isUnderline: Boolean
+})
+
+
+const emit = defineEmits(["update", "applyStyle"]);
 
 function handleUpdate(updatedNote) {
-  emit('update', updatedNote)
+  emit("update", updatedNote);
 }
 
 function handleStyle(styleType) {
-  emit('applyStyle', styleType)
+  emit("applyStyle", styleType);
 }
 </script>
 
 <template>
   <div class="toolbar">
     <HeadingControls :note="note" @update="handleUpdate" />
-    <TextStyleControls @applyStyle="handleStyle" />
+    <TextStyleControls
+      :isBold="isBold"
+      :isItalic="isItalic"
+      :isUnderline="isUnderline"
+      @applyStyle="handleStyle"
+    />
+
     <SpellcheckToggle />
   </div>
 </template>
