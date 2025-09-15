2fada62ab7b140c5a1a38cef4ecff26ad4eb4ca7
Banu Ağgün <banuaggun@outlook.com>
2025-09-15 17:53:59 +0300

    Add props and active class support for text styles (bold, italic, underline)

--- src/components/toolbar/TextStyleControls.vue
+++ src/components/toolbar/TextStyleControls.vue
@@ -1,16 +1,31 @@
 <script setup>
-const emit = defineEmits(['applyStyle'])
+const props = defineProps({
+  isBold: Boolean,
+  isItalic: Boolean,
+  isUnderline: Boolean
+})
+const emit = defineEmits(["applyStyle"]);
 
 function apply(styleType) {
-  emit('applyStyle', styleType)
+  emit("applyStyle", styleType);
 }
 </script>
 
 <template>
   <div class="text-style-controls">
-    <button @click="apply('bold')"><strong>B</strong></button>
-    <button @click="apply('italic')"><em>I</em></button>
-    <button @click="apply('underline')"><u>U</u></button>
+
+    <button @click="apply('bold')" :class="['bold-btn', { active: props.isBold }]">
+  <strong>B</strong>
+</button>
+
+<button @click="apply('italic')" :class="['italic-btn', { active: props.isItalic }]">
+  <em>I</em>
+</button>
+
+<button @click="apply('underline')" :class="['underline-btn', { active: props.isUnderline }]">
+  <u>U</u>
+</button>
+
   </div>
 </template>
 
@@ -22,4 +37,12 @@ function apply(styleType) {
   background-color: #fff;
   cursor: pointer;
 }
+.text-style-controls button.bold-btn.active,
+.text-style-controls button.italic-btn.active,
+.text-style-controls button.underline-btn.active{
+  background-color: fuchsia !important;
+  border-color: #999;
+}
+
+
 </style>
