e94934572bf37798484c9301f8c1eace5665d7d6
Banu Ağgün <banuaggun@outlook.com>
2025-10-28 20:46:38 +0300

    sidebar file location edited

---
+++ src/components/sidebar/Sidebar.vue
@@ -0,0 +1,35 @@
+<template>
+    <aside class="sidebar">
+        <nav class="sidebar-nav">
+            <div class="sidebar-nav-links">
+                <router-link to="/" class="nav-item" :class="{active:$route.path === '/'}">
+                    All Notes
+                </router-link>
+
+                <router-link to="/archived" class="nav-item" :class="{active:$route.path === '/archived'}">
+                    Archived Notes
+                </router-link>
+            </div>
+        </nav>
+    </aside>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+.sidebar{
+    width:200px;
+    height:100vh;
+    border:1px solid red;
+}
+.sidebar-nav-links{
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    gap:20px;
+    margin:20px 0 0 30px;
+}
+</style>
