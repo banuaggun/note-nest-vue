eaaf2b78ee6242a38098196a57e2ecf657e171c6
Banu Ağgün <banuaggun@outlook.com>
2025-10-28 20:47:39 +0300

    List views for archived notes and all notes have been edited.

---
+++ src/views/ArchivedNotes.vue
@@ -0,0 +1,20 @@
+<script setup>
+import NoteList from '../components/notes/NoteList.vue'
+
+const selectedNote = defineModel('selectedNote')
+</script>
+
+<template>
+  <NoteList 
+  :showArchived="true"
+    :modelValue="selectedNote"
+    @update:modelValue="selectedNote = $event"
+  />
+</template>
+
+<style scoped>
+.note-list-panel {
+  width: 100%;
+  height: 100%;
+}
+</style>
